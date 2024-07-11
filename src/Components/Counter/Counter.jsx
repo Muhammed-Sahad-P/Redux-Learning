610881b4fe7168b398f2d30f3849e32397df697f
Muhammed Sahad P <zanusahad@gmail.com>
2024-07-11 17:09:06 +0530

    Add style

--- src/Components/Counter/Counter.jsx
+++ src/Components/Counter/Counter.jsx
@@ -1,14 +1,44 @@
 import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "./CounterSlice";
+import { increment, decrement, Reset, incrementbyvalue } from "./CounterSlice";
+import "./Counter.css";
 
 const Counter = () => {
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
+
   return (
-    <div>
-      <h1>{count}</h1>
-      <button onClick={() => dispatch(increment())}>+</button>
-      <button onClick={() => dispatch(decrement())}>-</button>
+    <div className="counter-container">
+      <h1 className="counter-value">{count}</h1>
+      <div className="counter-buttons">
+        <button
+          className="counter-button"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <button className="counter-button" onClick={() => dispatch(Reset())}>
+          Reset
+        </button>
+        <button
+          className="counter-button"
+          onClick={() => dispatch(incrementbyvalue(5))}
+        >
+          Inc by 5
+        </button>
+
+        <button
+          className="counter-button"
+          onClick={() => dispatch(incrementbyvalue(-5))}
+        >
+          dec by 5
+        </button>
+        <button
+          className="counter-button"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
     </div>
   );
 };
